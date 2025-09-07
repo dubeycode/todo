d262575dd46052103958199019e952e48245637b
dubeycode <dubeysatyam3635@gmail.com>
2025-09-07 23:27:55 +0530

    add some comment

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import styles from './Input.module.css'
 import '../App.css'
 export const Input = () => {
     return (
         <>
+        {/* input for to do list  */}
             <div className="container text-center">
                 <div className="row">
                     <div className="col">
-                        <input type="text" className='input' placeholder='Enter ToDo'/>
+                        <input type="text" className={styles.input} placeholder='Enter ToDo'/>
                     </div>
                     <div className="col">
-                        <input type="Date" className='input' />
+                        <input type="Date" className={styles.input} />
                     </div>
+                    {/* sucess button */}
                     <div className="col">
                         <button type="button" className="btn btn-success">Success</button>
                     </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import styles from './Todo.module.css'
 
 export const Todo = () => {
   return (
-    <div><center><h1>Todo App</h1></center></div>
+    // hadding for to do list 
+    <div><center><h1 className={styles.todoHeading}>Todo App</h1></center></div>
   )
 }
